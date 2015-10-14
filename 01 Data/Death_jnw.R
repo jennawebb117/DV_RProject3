c858e618e839c450ad7097b57b4efa6378ad448f
jennawebb117 <jennawebb@utexas.edu>
2015-10-13 20:17:58 -0500

    added stuff to Death_jnw.R

--- 01 Data/Death_jnw.R
+++ 01 Data/Death_jnw.R
@@ -1,5 +1,6 @@
 require("jsonlite")
 require("RCurl")
+require(dplyr)
 
 df_France <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from FranceDeaths"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_jnw653', PASS='orcl_jnw653', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
