bd0c0fb6de437cee538575eaecefd9dfdcfec077
jennawebb117 <jennawebb@utexas.edu>
2015-10-15 22:27:25 -0500

    added stuff to Rmd, added stuff to my data wrangling

--- 02 Data Wrangling/full_data_wrangle.R
+++ 02 Data Wrangling/full_data_wrangle.R
@@ -3,6 +3,11 @@ require(dplyr)
 require(ggplot2)
 require(jsonlite)
 require(RCurl)
+library(gridExtra)
 
-df_full %>% select(YEAR, SEX.x, SEX.y, CAUSE, CAUSEOFDEATH, COUNT, VALUE) %>% filter(CAUSEOFDEATH == "INTENTIONAL SELF-HARM (SUICIDE)", CAUSE == "Intentional self-harm") %>% ggplot(aes(x = YEAR, y = VALUE, color = SEX.x)) + geom_point(position = "jitter") + facet_wrap(~SEX.x)  + ggtitle("Title") 
+g1 <- df_full %>% select(YEAR, SEX.x, SEX.y, CAUSE, CAUSEOFDEATH, COUNT, VALUE) %>% filter(CAUSEOFDEATH == "INTENTIONAL SELF-HARM (SUICIDE)", CAUSE == "Intentional self-harm") %>% ggplot(aes(x = YEAR, y = VALUE, color = SEX.x)) + geom_point(position = "jitter") + facet_wrap(~SEX.x) + ggtitle("France Suicides") 
 
+g2 <- df_full %>% select(YEAR, SEX.x, SEX.y, CAUSE, CAUSEOFDEATH, COUNT, VALUE) %>% filter(CAUSEOFDEATH == "INTENTIONAL SELF-HARM (SUICIDE)", CAUSE == "Intentional self-harm") %>% ggplot(aes(x = YEAR, y = COUNT, color = SEX.y)) + geom_point(position = "jitter") + facet_wrap(~SEX.y) + ggtitle("New York Suicides") 
+
+g1
+g2
