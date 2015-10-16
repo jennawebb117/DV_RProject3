5e170745d81843e6b1f93e80268f34ccd5c86602
jennawebb117 <jennawebb@utexas.edu>
2015-10-16 11:59:02 -0500

    put my stuff in the rmd, created basic skeleton for the rmd, knitted

--- 02 Data Wrangling/full_data_wrangle.R
+++ 02 Data Wrangling/full_data_wrangle.R
@@ -5,9 +5,9 @@ require(jsonlite)
 require(RCurl)
 library(gridExtra)
 
-g1 <- df_full %>% select(YEAR, SEX.x, SEX.y, CAUSE, CAUSEOFDEATH, COUNT, VALUE) %>% filter(CAUSEOFDEATH == "INTENTIONAL SELF-HARM (SUICIDE)", CAUSE == "Intentional self-harm") %>% ggplot(aes(x = YEAR, y = VALUE, color = SEX.x)) + geom_point(position = "jitter") + facet_wrap(~SEX.x) + ggtitle("France Suicides") 
+g1 <- df_full %>% select(YEAR, SEX.x, SEX.y, CAUSE, CAUSEOFDEATH, COUNT, VALUE) %>% filter(CAUSEOFDEATH == "INTENTIONAL SELF-HARM (SUICIDE)", CAUSE == "Intentional self-harm") %>% ggplot(aes(x = YEAR, y = VALUE, color = CAUSE)) + geom_point(position = "jitter") + facet_wrap(~SEX.x) + theme(axis.text.x=element_text(angle=70, size = 10, vjust=0.5)) + ggtitle("France Suicides") 
 
-g2 <- df_full %>% select(YEAR, SEX.x, SEX.y, CAUSE, CAUSEOFDEATH, COUNT, VALUE) %>% filter(CAUSEOFDEATH == "INTENTIONAL SELF-HARM (SUICIDE)", CAUSE == "Intentional self-harm") %>% ggplot(aes(x = YEAR, y = COUNT, color = SEX.y)) + geom_point(position = "jitter") + facet_wrap(~SEX.y) + ggtitle("New York Suicides") 
+g2 <- df_full %>% select(YEAR, SEX.x, SEX.y, CAUSE, CAUSEOFDEATH, COUNT, VALUE) %>% filter(CAUSEOFDEATH == "INTENTIONAL SELF-HARM (SUICIDE)", CAUSE == "Intentional self-harm") %>% ggplot(aes(x = YEAR, y = COUNT, color = CAUSEOFDEATH)) + geom_point(position = "jitter") + facet_wrap(~SEX.y) + theme(axis.text.x=element_text(angle=70, size = 10, vjust=0.5)) + ggtitle("New York Suicides") 
 
 g1
 g2
