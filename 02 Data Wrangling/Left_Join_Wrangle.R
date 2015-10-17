7045000cfe5d2cbdf2edf30fcf5bbbc875ffd6dc
jennawebb117 <jennawebb@utexas.edu>
2015-10-16 23:36:18 -0500

    .

--- 02 Data Wrangling/Left_Join_Wrangle.R
+++ 02 Data Wrangling/Left_Join_Wrangle.R
@@ -6,4 +6,3 @@ require(RCurl)
 
 Plot <- df_bind %>% select(YEAR, CAUSEOFDEATH, COUNT, FRANCE_SEX, NY_SEX) %>% filter(CAUSEOFDEATH == "ACCIDENTS EXCEPT DRUG POISONING")%>% filter(FRANCE_SEX != "NA" & NY_SEX != "NA") %>% ggplot(aes(x = NY_SEX, y = FRANCE_SEX, color = COUNT)) + ggtitle("Accidents as Cause Death in France and New York 2007-2011 ") + geom_point(position = "jitter")
 
-Plot
