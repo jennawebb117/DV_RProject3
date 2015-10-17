170000c4f800749d907d608feb00979088645bdc
Happy Situ <happysitu@utexas.edu>
2015-10-16 23:03:42 -0500

    changes

--- 01 Data/Death_hys.R
+++ 01 Data/Death_hys.R
@@ -4,11 +4,3 @@ dfFrance <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/nativ
 dfNYC <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from DeathsNYC"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
 innerj <- dplyr::inner_join(dfFrance, dfNYC, by="YEAR")
-
-
-innerj %>% select(YEAR, ETHNICITY, SEX.x, SEX.y, CAUSE, CAUSEOFDEATH, COUNT, VALUE) %>% filter(CAUSEOFDEATH == "ASSAULT (HOMICIDE)", CAUSE == "Assault") %>% ggplot(aes(x = YEAR, y = VALUE, color = SEX.x)) + geom_point(position = "jitter") + theme(axis.text.x=element_text(angle=70, size = 10, vjust=0.5)) + ggtitle("France Homicide")
-
-innerj %>% select(YEAR, ETHNICITY, SEX.x, SEX.y, CAUSE, CAUSEOFDEATH, COUNT, VALUE) %>% filter(CAUSEOFDEATH == "ASSAULT (HOMICIDE)", CAUSE == "Assault") %>% ggplot(aes(x = YEAR, y = COUNT, color = SEX.y)) + geom_point(position = "jitter") + theme(axis.text.x=element_text(angle=70, size = 10, vjust=0.5)) + ggtitle("NYC Homicide")
-
-                                                        
-                     
